8a9159b1f8bb909a7b0cdae7b78b20c1fa05ec34
Htet Myat Hlaing <htetmyathlaing.htd@gmail.com>
2022-09-04 17:02:20 +0630

    function & object part 2

--- javascript-basic/js/function.js
+++ javascript-basic/js/function.js
@@ -52,4 +52,99 @@ result = add(2, 4)
 console.log('add ', result)
 console.log('add without arguments', add())
 
+// A little dive into this keyword
+
+// Invoked as a function
+function test(){
+    console.log('this === window', this === window)
+}
+console.log('this === window', this === window)
+test()
+
+// Invoked as a method
+const person = {
+    name: 'Person',
+    getName: function(){
+        // console.log('this === person', this === person)
+        return this.name
+    }
+}
+console.log(person.getName())
+
+// as a constructor
+function Person(name){
+    this.name = name
+}
+
+let p1 = new Person('Mg Hla')
+let p2 = new Person('Ma Mya')
+console.log(p1.name)
+console.log(p2.name)
+
+
+// via call , apply, bind
+let person1 = {
+    name: 'Person 1'
+}
+
+let person2 = {
+    name: 'Person 2'
+}
+
+let person3 = {
+    name: 'Person 3'
+}
+
+console.log(person.getName())
+console.log(person.getName.call(person1))
+console.log(person.getName.apply(person2))
+console.log(person.getName.bind(person3)())
+
+
+console.log(add(1, 5))
+console.log(add.apply(null, [3, 7]))
+console.log(add.call(null, 4, 8))
+
 // Closure
+
+let globalScopeVariable = 'Global';
+
+function someFunction(){
+    let functionScopeVariable = 'Function'
+    console.log(functionScopeVariable)
+    console.log(globalScopeVariable)
+}
+
+console.log(globalScopeVariable)
+// console.log(functionScopeVariable) // will throw error
+someFunction()
+
+function Counter(){
+    let counter = 0
+    this.add = function(){
+        counter++
+        return counter
+    }
+}
+
+let counter = new Counter();
+counter.add()
+counter.add()
+console.log(counter.add())
+
+
+let count = (function(){
+    let counter = 0 
+    return function(){
+        counter ++
+        return counter;
+    }
+})();
+
+count()
+count()
+console.log(count())
+
+
+
+
